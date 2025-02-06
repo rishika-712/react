9f56e9f97ee1a77b3a66ee55d02a7c54b2a834ed
rishika-712 <angarishika8@gmail.com>
2025-02-06 12:56:01 +0530

    pushing files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,78 @@
 
 
 // 
-import React from 'react';
-import Navbar from './components/Navbar';
+// import React from 'react';
+// import Navbar from './components/Navbar';
 
-import { Main } from './components/Main';
-import { Sidebar1 } from './components/Sidebar1';
-import { Sidebar2 } from './components/Sidebar2';
-import { Footer } from './components/Footer';
-const App = () => {
-  return (
-    <div className='app'>
-      <Navbar />
-      <Main />
-      <div className='sidebar'>
-        <Sidebar1 />
-        <Sidebar2 />
+// import { Main } from './components/Main';
+// import { Sidebar1 } from './components/Sidebar1';
+// import { Sidebar2 } from './components/Sidebar2';
+// import { Footer } from './components/Footer';
+// const App = () => {
+//   return (
+//     <div className='app'>
+//       <Navbar />
+//       <Main />
+//       <div className='sidebar'>
+//         <Sidebar1 />
+//         <Sidebar2 />
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default App;
+// import React, { Component } from 'react'
+
+// import FBCPropEx from './propsex/FBCPropEx';
+
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         {/* <CBCPropEx
+//         username="rishika"
+//         age={20}
+//         hobbies={["playing" , "reading"]}
+//         address={{city:"hyderabad", area:"kompally"}}
+//         sendFun={function(){alert("hi i am rishika")}}
+//         /> */}
+//          <FBCPropEx
+//           username="rishika"
+//           isMarried={false}
+//           hobbies={["Chatting","Reading","Eating","Sleeping"]}
+//         />
+//       </div>
+//     );
+//   }
+// }
+import React, { Component } from 'react'
+class CBCPropEx extends Component {
+  constructor(){
+    super();
+    this.state={
+      users:["Rishika" , "615" , "Varshini" , "sadvi"],
+      num:100
+    };
+  }
+  changeNumber=()=>{
+    this.setState({num:200})
+  }
+  render() {
+    console.log(this);
+    return (
+      <div>
+        {
+          this.state.users.map((user , i)=>{
+            return <li key={i}>{user}</li>
+          })
+        }
+        <h1>{this.state.num}</h1>
+        <button onClick={this.changeNumber}>Change number</button>
       </div>
-      <Footer />
-    </div>
-  );
-};
-
-export default App;
-
+    )
+  }
+}
 
